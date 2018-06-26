a9612fe91de944bbcb27326090fce3eb0936563d
BrendanLindsay <brendan.g.lindsay@gmail.com>
2018-06-26 19:51:15 -0400

    functions are wild

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ﻿### Udemy JavaScript Course
 
-I took this course to receive a fundamental knowledge of vanilla JavaScript. The course begins by teaching the basics of JavaScript syntax and then walks you through multiple excercises, challenges, and even a final project.
+I took this course to receive a fundamental knowledge of vanilla JavaScript. The course begins by teaching the basics of JavaScript syntax and then walks you through multiple exercises, challenges, and even a final project.
 
 I chose to take Jonas' course as I previously took his HTML & CSS course and found it extremely helpful, as he walks you through line by line explaining good and bad form, and revisits previous exercises as you learn so that you may improve your previous code using the new knowledge you have.
 
-I also chose his course as it teaches without the use of jQuery, so that i can know the benefits of jQuery better, while still understanding the code that the jQuery shortcuts are replacing.
+I also chose his course as it teaches without the use of jQuery, so that I may know the benefits of jQuery better, while still understanding the code that the jQuery shortcuts are replacing.
 
-## <a href="https://www.udemy.com/the-complete-javascript-course/learn/v4/overview">Link to the course</a>
+# <a href="https://www.udemy.com/the-complete-javascript-course/learn/v4/overview">Link to the course</a>
--- script.js
+++ script.js
@@ -375,15 +375,111 @@ yearsUntilRetirement(1969, 'Jane');
 /*****************************
  Function statements and expressions
 *****************************/
+/*
+// Function declaration
+// function whatDoYouDo(job, firstName) {}
 
+// Function expression
+var whatDoYouDo = function(job, firstName) {
+    switch (job) {
+        case 'teacher':
+            return firstName + ' teaches kids how to code.';
+        case 'driver':
+            return firstName + ' drives uber in Charlotte.';
+        case 'designer':
+            return  firstName + ' designs websites.';
+        default:
+            return firstName + ' is a ' + job;
+    }
+}
 
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('driver', 'Jane'));
+console.log(whatDoYouDo('designer', 'Mark'));
+console.log(whatDoYouDo('painter', 'Matt'));
+*/
+/*****************************
+ Arrays
+*****************************/
+/*
+// Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
 
+console.log(names[0], years[0]);
+console.log(names.length);
 
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary';
 
+console.log(names);
 
+// Different data types
+var john = ['John', 'Smith', 1990, 'teacher', false];
+john.push('blue');
+john.unshift('Mr');
 
+console.log(john);
 
+john.pop();
 
+console.log(john);
+
+john.shift();
+
+console.log(john);
+
+console.log(john.indexOf(1990));
+
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a desinger' : 'John IS a designer';
+console.log(isDesigner);
+*/
+/*****************************
+ CODING CHALLENGE 3
+*****************************/
+/*
+John and his family went on a holiday and went to 3 different restaurants. The bills were $124, $48 and $268.
+
+To tip the waiter a fair amount, John created a simple tip calculator (as a function). He likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+In the end, John would like to have 2 arrays:
+1) Containing all three tips (one for each bill)
+2) Containing all three final paid amounts (bill + tip).
+
+(NOTE: To calculate 20% of a value, simply multiply it with 20/100 = 0.2)
+
+GOOD LUCK 😀
+*/
+/*
+function tipCalculator(bill) {
+    var percentage;
+    if (bill < 50) {
+        percentage = .2;
+    } else if (bill >= 50 && bill < 200) {
+        percentage = .15;
+    } else if (bill >= 200) {
+        percentage = .1;
+    }
+    return percentage * bill;
+}
+
+var bills = [124, 48, 268];
+
+var tips = [tipCalculator(bills[0]),
+            tipCalculator(bills[1]),
+            tipCalculator(bills[2])];
+
+var total = [tips[0] + bills[0],
+             tips[1] + bills[1],
+             tips[2] + bills[2]];
+
+console.log(tips);
+console.log(total);
+*/
+/*****************************
+ Objects and properties
+*****************************/
 
 
 
