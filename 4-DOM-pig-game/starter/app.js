d6fac550421daf9d46bc7700c11e935d5cfd32e4
BrendanLindsay <brendan.g.lindsay@gmail.com>
2018-07-30 20:05:23 -0400

    DOM and Acces Manipulation

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,6 +9,22 @@ GAME RULES:
 
 */
 
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+console.log(dice);
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
+document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+
+
+
 
 
 
