399572df6eb5e64e69df6023b1fa728bf09abb9a
BrendanLindsay <brendan.g.lindsay@gmail.com>
2018-07-30 20:58:14 -0400

    Event handling

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,24 +9,59 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-console.log(dice);
-
 //document.querySelector('#current-' + activePlayer).textContent = dice;
-document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
 
+//var x = document.querySelector('#score-0').textContent;
+//console.log(x);
 
+document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
+// Event handling
 
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    //1. Random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    //2. Display the result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    //3. Update the round score if the rolled number was not a 1 (b/c rolling a 1 means your turn is up)
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        //Changing the active player styling
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //When 1 is rolled, hide the dice again
+        document.querySelector('.dice').style.display = 'none';
+    }
+});
 
 
 
