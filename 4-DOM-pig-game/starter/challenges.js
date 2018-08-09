b9b984b7ec897e955143157a4fe9cd4d8df3b46b
BrendanLindsay <brendan.g.lindsay@gmail.com>
2018-08-08 20:27:42 -0400

    completed final dice game coding challenge

--- 4-DOM-pig-game/starter/challenges.js
+++ 4-DOM-pig-game/starter/challenges.js
@@ -16,14 +16,17 @@ var lastDice;
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if (gamePlaying) {
         //1. Random number
-        var dice = Math.floor(Math.random() * 6) + 1;
+        var dice1 = Math.floor(Math.random() * 6) + 1;
+        var dice2 = Math.floor(Math.random() * 6) + 1;
 
         //2. Display the result
-        var diceDOM = document.querySelector('.dice');
-        diceDOM.style.display = 'block';
-        diceDOM.src = 'dice-' + dice + '.png';
+        document.getElementById('dice-1').style.display = 'block';
+        document.getElementById('dice-2').style.display = 'block';
+        document.getElementById('dice-1').src = 'dice-' + dice1 + '.png';
+        document.getElementById('dice-2').src = 'dice-' + dice2 + '.png';
         console.log(gamePlaying);
         
+        /*
         //3. Update the round score if the rolled number was not a 1 (b/c rolling a 1 means your turn is up)
         if (dice === 6 && lastDice === 6) {
             //Player loses score, as they rolled two sixes in a row
@@ -40,8 +43,21 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         }
         // Coding challenge
         lastDice = dice;
+        */
+
+       //Coding challenge part 3: two dice
+       if (dice1 !== 1 && dice2 !== 1) {
+        //Add score
+        roundScore += dice1 + dice2;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+        //Next player
+        nextPlayer();
+        }
+        
     }
 });
+
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
     if(gamePlaying) {
@@ -54,19 +70,21 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
         //If yes, assign that number to winningScore
         //If not, default is 100
         var input = document.querySelector('.final-score').value;
-        
+        var winningScore;
+
         if(input) {
-            var winningScore = input;
+            winningScore = input;
         } else {
             winningScore = 100;
         }
 
         //Check if player won game
-        if (scores[activePlayer] >= 50) {
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
             document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
             document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('winner');
-            document.querySelector('.dice').style.display = 'none';
+            document.getElementById('dice-1').style.display = 'none';
+            document.getElementById('dice-2').style.display = 'none';
             gamePlaying = false;
         } else {
             //Next player
@@ -88,7 +106,8 @@ function nextPlayer() {
         document.querySelector('.player-1-panel').classList.toggle('active');
 
         //When 1 is rolled, hide the dice again
-        document.querySelector('.dice').style.display = 'none';
+        document.getElementById('dice-1').style.display = 'none';
+        document.getElementById('dice-2').style.display = 'none';
 }
 
 
@@ -100,7 +119,11 @@ function init() {
     roundScore = 0;
     gamePlaying = true;
 
-    document.querySelector('.dice').style.display = 'none';
+    document.getElementById('dice-1').style.display = 'none';
+    document.getElementById('dice-2').style.display = 'none';
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
 
     document.getElementById('score-0').textContent = '0';
     document.getElementById('score-1').textContent = '0';
