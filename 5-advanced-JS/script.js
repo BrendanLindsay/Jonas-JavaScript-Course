5a79f5d25db9f6c8602204b43e482d7c972de6f6
BrendanLindsay <brendan.g.lindsay@gmail.com>
2018-08-15 21:02:59 -0400

    object.create

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -1,8 +1,71 @@
 // Function constructor
-
+/*
 var john = {
     name: 'John',
     yearOfBirth: 1990,
     job: 'teacher'
 };
+
+var Person = function(name, yearOfBirth, job) {
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+}
+
+Person.prototype.calculateAge = function() {
+    console.log(2016 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Smith';
+
+var john = new Person('John', 1990, 'teacher');
+var jane = new Person('Jane', 1969, 'designer');
+var mark = new Person('Mark', 1940, 'retired');
+
+console.log(john.lastName);
+
+john.calculateAge();
+mark.calculateAge();
+jane.calculateAge();
+*/
+
+
+// Object.create
+var personProto = {
+    calculateAge: function() {
+        console.log(2016 - this.yearOfBirth);
+        
+    }
+};
+
+var john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = 1990;
+john.job = 'teacher';
+
+var jane = Object.create(personProto,
+{
+    name: { value: 'Jane' },
+    yearOfBirth: { value: 1969},
+    job: { value: 'designer'},
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
