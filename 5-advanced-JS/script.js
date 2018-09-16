6f2daafacb3a272db87a8d0b32320d757266e223
BrendanLindsay <brendan.g.lindsay@gmail.com>
2018-09-16 15:34:44 -0400

    functions as arguments

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -91,6 +91,39 @@ console.log(obj.city);
 
 // Passing functions as arguments
 
+var years = [1991, 1965, 1937, 2005, 1998];
+
+function arrayCalc(arr, fn) {
+    var arrRes = [];
+    for (var i = 0; i < arr.length; i++) {
+        arrRes.push(fn(arr[i]));
+    }
+    return arrRes;
+}
+
+function calculateAge(el) {
+    return 2016 - el;
+}
+
+function isFullAge(el) {
+    return el >= 18;
+}
+
+function maxHeartRate(el) {
+    if (el >= 18 && el <= 81) {
+    return Math.round(206.9 - (0.67 * el));
+    } else {
+        return 'Too young';
+    }
+}
+
+var ages = arrayCalc(years, calculateAge);
+var fullAges = arrayCalc(ages, isFullAge);
+var rates = arrayCalc(ages, maxHeartRate);
+
+console.log(ages);
+console.log(fullAges);
+console.log(rates);
 
 
 
