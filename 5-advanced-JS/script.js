b3bc14fd587e51f793f57649ecd923c69835dcbb
BrendanLindsay <brendan.g.lindsay@gmail.com>
2018-09-16 14:34:12 -0400

    object create

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -44,12 +44,11 @@ john.yearOfBirth = 1990;
 john.job = 'teacher';
 
 var jane = Object.create(personProto,
-{
-    name: { value: 'Jane' },
-    yearOfBirth: { value: 1969},
-    job: { value: 'designer'},
-})
-
+    {
+        name: { value: 'Jane' },
+        yearOfBirth: { value: '1969' },
+        job: { value: 'designer' },
+    });
 
 
 
