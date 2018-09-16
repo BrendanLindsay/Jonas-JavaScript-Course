db14bf10266f0bf47000b1c85c01631795391a2a
BrendanLindsay <brendan.g.lindsay@gmail.com>
2018-09-16 15:14:55 -0400

    function changing

--- 5-advanced-JS/script.js
+++ 5-advanced-JS/script.js
@@ -31,6 +31,7 @@ jane.calculateAge();
 
 
 // Object.create
+/*
 var personProto = {
     calculateAge: function() {
         console.log(2016 - this.yearOfBirth);
@@ -49,6 +50,50 @@ var jane = Object.create(personProto,
         yearOfBirth: { value: '1969' },
         job: { value: 'designer' },
     });
+*/
+/*
+// Primitives vs Objects
+
+var a = 23;
+var b = a;
+a = 46;
+
+
+
+// Objects
+var obj1 = {
+    name: 'John',
+    age: 26
+};
+
+var obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+// Functions
+var age = 27;
+var obj = {
+    name: 'Jonas',
+    city: 'Lisbon'
+};
+
+function change(a, b) {
+    a = 30,
+    b.city = 'San Francisco'
+};
+
+change(age, obj);
+
+console.log(age);
+console.log(obj.city);
+*/
+
+// Passing functions as arguments
+
+
+
+
 
 
 
